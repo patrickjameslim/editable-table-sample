8d8a45fa343a313efeac07b950b40ba783f46782
jmramos02 <ramosjosemari@gmail.com>
2014-10-04 20:47:28 +0800

    Fixed Error on schema, user_id not defined

--- app/database/migrations/2014_09_30_043245_audit_trails_table.php
+++ app/database/migrations/2014_09_30_043245_audit_trails_table.php
@@ -14,6 +14,7 @@ class AuditTrailsTable extends Migration {
 	{
 		Schema::create('audit_trails',function($table){
 			$table->increments('id');
+			$table->integer('user_id')->unsigned();
 			$table->foreign('user_id')->references('id')->on('users');
 			$table->string('activity');
 			$table->timestamps();
